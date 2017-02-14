098645f365c029282f640853290586e453e48101
Awa <awa.assahna@andela.com>
2017-02-14 11:27:27 +0100

    setup app for heroku deployment

--- config/env/all.js
+++ config/env/all.js
@@ -5,5 +5,5 @@ var keys = rootPath + '/keys.txt';
 module.exports = {
 	root: rootPath,
 	port: process.env.PORT || 3000,
-    db: process.env.MONGOHQ_URL
+    db: 'mongodb://<dbuser>:<dbpassword>@ds139705.mlab.com:39705/izanagi-cfh'
 };
