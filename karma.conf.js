940557a7c04c9348d20e4128e0b98439b95b237e
Andela <andela@Andelas-MacBook-Pro.local>
2017-02-22 14:02:52 +0100

    chore(karma): modify karmand travis file [Fixes ##139456313]

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      './test/frontend/**/*.spec.js'
+      './test/frontend/*.js'
     ],
 
 
@@ -26,14 +26,20 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
+    // preprocessors: {
+    //     './test/frontend/**/*.spec.js': ['coverage']
+    // },
+
+    coverageReporter :{
+        type: 'lcov',
+        dir: 'coverage/'
     },
 
-
+    
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['spec' , 'coverage', 'coveralls', 'verbose'],
 
 
     // web server port
