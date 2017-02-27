6f637e830cc0a2cf4b279c2fa8422c289382552c
redd <rowland.henshaw@andela.com>
2017-02-27 15:36:38 +0100

    changed db config settings in server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
     mongoose = require('mongoose');
 
 //Bootstrap db connection
-var db = mongoose.connect(process.env.MONGOHQ_URL);
+var db = mongoose.connect(config.db);
 
 //Bootstrap models
 var models_path = __dirname + '/app/models';
